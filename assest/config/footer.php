3660cc1cdeb6f266ee74a6f05dd2071ceecb7332
Michael Alexis Salas Gonzalez <michael@mscode.cl>
2023-12-25 22:06:48 -0300

    Versionado remoto

--- assest/config/footer.php
+++ assest/config/footer.php
@@ -1,11 +1,38 @@
+<?php 
+$api_url = 'https://mscode.cl/api/version.php?getVersionNumber'; 
+
+// Realizar la solicitud GET a la API
+$curl = curl_init($api_url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$api_response = curl_exec($curl);
+curl_close($curl);
+
+// Decodificar la respuesta JSON
+$api_data = json_decode($api_response, true);
+
+$localVersion = "1.1.10";
+$remoteVersion = $api_data['version'];
+
+
+?>
 <footer class="main-footer">
     <div class="pull-right d-none d-sm-inline-block">
         <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
 		  <li class="nav-item">
-			<a class="nav-link" href="javascript:void(0)">Ultima Actualización</a>
+			<a class="nav-link" href="javascript:void(0)">
+				<?php 
+
+if($remoteVersion === $localVersion){
+	echo '<span class="badge bg-success">Actualizado</span>';
+}else{
+	echo '<span class="badge bg-danger">Tiene una actualizaciòn pendiente!</span>';
+}
+				
+				?>
+			</a>
 		  </li>
 		  <li class="nav-item">
-			<a class="nav-link" href="#">Version 3.1 02-2022</a>
+			<a class="nav-link" href="#">Version <?php echo $localVersion; ?></a>
 		  </li>
 		</ul>
     </div>
