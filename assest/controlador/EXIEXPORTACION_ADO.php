3d659a4471e8829a15f248178e52470c9ced4f0e
Michael Alexis Salas Gonzalez <michael@mscode.cl>
2024-04-24 21:22:12 -0400

    Update EXIEXPORTACION_ADO.php

--- assest/controlador/EXIEXPORTACION_ADO.php
+++ assest/controlador/EXIEXPORTACION_ADO.php
@@ -2093,7 +2093,7 @@ class EXIEXPORTACION_ADO
                                                         AND ID_PLANTA = '" . $PLANTA . "'
                                                         AND ID_TEMPORADA = '" . $TEMPORADA . "'  
                                                         AND ESTADO_REGISTRO = 1
-                                                        AND ESTADO = 1                                               
+                                                        AND ESTADO = 2                                               
                                                 GROUP BY FOLIO_AUXILIAR_EXIEXPORTACION
                                           ;");
             $datos->execute();
